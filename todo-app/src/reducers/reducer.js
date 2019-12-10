ecdd66ab965d43038fcbcf6b415e3228a46d427e
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-10 17:24:58 -0500

    step 1 done

---
+++ todo-app/src/reducers/reducer.js
@@ -0,0 +1,22 @@
+export const initialState = [
+    {
+        item: "learn about reducers",
+        completed: false,
+        id: 1
+    },
+    {
+        item: "code some more",
+        completed: false,
+        id: 2
+    },
+    {
+        item: "workout",
+        completed: false,
+        id: 3
+    }
+]
+
+export const reducer = (state, action) => {
+
+    return state;
+}
